d86c178e05546c824872229584d51e4774ef2474
inspire-coding <+PaZ+PeTi+88113+>
2020-07-07 07:49:24 +0200

    Add the delete BMI feature

--- app/src/main/java/com/inspirecoding/bmicalculator/bmis/BmisFragment.kt
+++ app/src/main/java/com/inspirecoding/bmicalculator/bmis/BmisFragment.kt
@@ -5,9 +5,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.inspirecoding.bmicalculator.EventObserver
 import com.inspirecoding.bmicalculator.MainActivity
+import com.inspirecoding.bmicalculator.R
 import com.inspirecoding.bmicalculator.databinding.BmisFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -42,6 +46,25 @@ class BmisFragment : Fragment()
 
         onFabClick()
         setupListAdapter()
+        setupDeleteBmiDialog()
+    }
+
+    private fun setupDeleteBmiDialog()
+    {
+        viewModel.deleteBmiDialog.observe(viewLifecycleOwner, EventObserver { _bmi ->
+            context?.let { _context ->
+                val dialog = BottomSheetDialog(_context)
+                dialog.setContentView(this.layoutInflater.inflate(R.layout.dialog_bottomsheet_deleteitem,null))
+                dialog.findViewById<AppCompatButton>(R.id.btn_cancel)?.setOnClickListener {
+                    dialog.dismiss()
+                }
+                dialog.findViewById<AppCompatButton>(R.id.btn_delete)?.setOnClickListener {
+                    viewModel.deleteBmi((_bmi))
+                    dialog.dismiss()
+                }
+                dialog.show()
+            }
+        })
     }
 
     private fun setupListAdapter()
--- app/src/main/java/com/inspirecoding/bmicalculator/bmis/BmisViewModel.kt
+++ app/src/main/java/com/inspirecoding/bmicalculator/bmis/BmisViewModel.kt
@@ -1,15 +1,33 @@
 package com.inspirecoding.bmicalculator.bmis
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import android.view.View
+import androidx.lifecycle.*
+import com.inspirecoding.bmicalculator.Event
 import com.inspirecoding.bmicalculator.data.BmiDatabase
 import com.inspirecoding.bmicalculator.data.BmiRepository
 import com.inspirecoding.bmicalculator.data.BmiRepositoryImpl
 import com.inspirecoding.bmicalculator.model.BMI
+import kotlinx.coroutines.launch
 
 class BmisViewModel(private val bmiRepository: BmiRepository) : ViewModel()
 {
     val items: LiveData<List<BMI>> = bmiRepository.allBmi
+
+    private val _deleteBmiDialog = MutableLiveData<Event<BMI>>()
+    val deleteBmiDialog: LiveData<Event<BMI>> = _deleteBmiDialog
+
+    fun openDeleteBmiDialog(view: View, bmi: BMI): Boolean
+    {
+        _deleteBmiDialog.value = Event(bmi)
+
+        return false
+    }
+
+    fun deleteBmi(bmi: BMI)
+    {
+        viewModelScope.launch {
+            bmiRepository.deleteBmi(bmi)
+        }
+    }
 }
